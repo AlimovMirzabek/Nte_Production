d8429cd79d46b6d4cc2694d1bce5397287aebdfe
RedNut <75196496+RedNutt@users.noreply.github.com>
2021-09-22 15:28:28 +0600

    changed all files from KhasanAlimov

---
+++ src/js/utils.js
@@ -0,0 +1,27 @@
+// Добавить элемент
+const addElem = (parent, child, array) => {
+    const result = parent.appendChild(document.createElement(child));
+console.log(result)
+    array.forEach((element) => {
+        eval(`result.${element[0]} = element[1]`);
+    });
+
+    return result;
+};
+
+// Get DOM element
+const elem = (selector) => {
+    return document.querySelector(selector);
+};
+
+// Get DOM elements
+const elems = (selector) => {
+    return document.querySelectorAll(selector);
+};
+
+// Capitalize first letter
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export { addElem, elem, capitalizeFirstLetter, elems };
